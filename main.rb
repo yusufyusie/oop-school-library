6d9382c80e1dd460c1ec552a293ef1c2167b3943
dvcorreia1984 <victor@onsleer.co.za>
2023-10-21 11:47:56 +0200

    Added menu.rb file and updated main.rb file

--- main.rb
+++ main.rb
@@ -1,41 +1,39 @@
 require_relative 'app'
+require_relative 'menu'
 
-def main_menu
-  puts "\n\nWelcome to the School Library App!\n\n"
+def handle_menu_option(choice, books, people, rentals)
+  menu_actions = {
+    1 => -> { list_all_books(books) },
+    2 => -> { list_all_people(people) },
+    3 => -> { create_person(people) },
+    4 => -> { add_book(books) },
+    5 => -> { create_rental(books, people, rentals) },
+    6 => -> { list_rentals(rentals) },
+    7 => -> { exit_program }
+  }
+
+  action = menu_actions[choice]
+
+  action&.call
+end
+
+def main
+  books = []
+  people = []
+  rentals = []
+
+  Menu.display_welcome_message
 
   loop do
-    puts 'Please choose an option by entering a number:'
-    puts '1. List all books'
-    puts '2. List all people'
-    puts '3. Create a person'
-    puts '4. Create a book'
-    puts '5. Create a rental'
-    puts '6. List all rentals for a given person'
-    puts '7. Exit'
+    choice = Menu.display_menu_and_get_choice
 
-    choice = gets.chomp.to_i
-
-    case choice
-    when 1
-      list_all_books
-    when 2
-      list_all_people
-    when 3
-      create_person
-    when 4
-      add_book
-    when 5
-      create_rental
-    when 6
-      list_rentals
-    when 7
-      exit_program
+    if (1..7).include?(choice)
+      handle_menu_option(choice, books, people, rentals)
+      break if choice == 7
     else
-      puts 'Invalid choice'
+      puts 'Invalid choice. Please select a valid option (1-7).'
     end
-
-    break if choice == 7
   end
 end
 
-main_menu
+main
