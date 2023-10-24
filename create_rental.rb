578e8be0e13bdade25438b33337e0caef12c26be
dvcorreia1984 <victor@onsleer.co.za>
2023-10-24 14:52:17 +0200

    Revert "Rubocop changes"
    This reverts commit 771598db95010e10cd2c0a9e159de239101398f0.

--- create_rental.rb
+++ create_rental.rb
@@ -1,12 +1,12 @@
 def create_rental(books, people, rentals)
-  book = select_book(books)
-  person = select_person(people)
-
-  if book && person
-    date = rental_date
-    create_and_add_rental(rentals, date, book, person)
-    puts 'Rental created successfully'
-  else
-    puts 'Invalid input, please try again'
-  end
+    book = select_book(books)
+    person = select_person(people)
+  
+    if book && person
+      date = rental_date
+      create_and_add_rental(rentals, date, book, person)
+      puts 'Rental created successfully'
+    else
+      puts 'Invalid input, please try again'
+    end
 end
