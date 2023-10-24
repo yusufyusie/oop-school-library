fd5083d6e9d052c4aa8c0d46cac5d671b3157da4
dvcorreia1984 <victor@onsleer.co.za>
2023-10-24 10:41:44 +0200

    Update code

--- main.rb
+++ main.rb
@@ -1,8 +1,21 @@
-# main.rb
-
 require_relative 'app'
 require_relative 'menu'
-require_relative 'menu_handler'
+
+def handle_menu_option(choice, books, people, rentals)
+  menu_actions = {
+    1 => -> { list_all_books(books) },
+    2 => -> { list_all_people(people) },
+    3 => -> { create_person(people) },
+    4 => -> { add_book(books) },
+    5 => -> { create_rental(books, people, rentals) },
+    6 => -> { list_rentals(rentals) },
+    7 => -> { exit_program }
+  }
+
+  action = menu_actions[choice]
+
+  action&.call
+end
 
 def main
   books = []
