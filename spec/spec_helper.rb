e2a163a41e6c59f7b69e91aaf98a25fb733a8500
dvcorreia1984 <victor@onsleer.co.za>
2023-10-28 20:04:24 +0200

    Updated files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,17 @@
+$LOAD_PATH.unshift(File.expand_path('../', __FILE__))
+
 require 'rspec'
-require '../book'
-require '../classroom'
-require '../decorator'
-require '../person'
-require '../rental'
-require '../student'
-require '../teacher'
-require '../trimmer_decorator'
-require '../nameable'
-require '../capitalize_decorator'
-require_relative '../app'
 require_relative '../book'
+require_relative '../classroom'
+require_relative '../decorator'
+require_relative '../person'
+require_relative '../rental'
+require_relative '../student'
+require_relative '../teacher'
+require_relative '../trimmer_decorator'
+require_relative '../nameable'
+require_relative '../capitalize_decorator'
+require_relative '../app'
 
 RSpec.configure do |config|
   # Configure RSpec options and any other necessary settings
